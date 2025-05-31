4211429139eba6ed5428260cc907f6ea7a8f7d92
Anton Fayfer <fayfer2004@mail.ru>
2025-05-31 14:08:28 +0600

    fix pep8

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from foodgram import settings
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from api.views import redirect_by_hash
+from foodgram import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),

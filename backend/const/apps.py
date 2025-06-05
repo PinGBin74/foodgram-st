2a05e672ccc9b5f9f9aa78f70a8550db55977e81
Anton Fayfer <fayfer2004@mail.ru>
2025-06-05 14:03:18 +0600

    delete backend

--- backend/const/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ConstConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "const"
--- backend/users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    search_fields = ("email", "username")
-    list_display = ("id", "email", "username", "first_name", "last_name", "password")
-    search_help_text = "Поиск по электронной почте и никнейму"
